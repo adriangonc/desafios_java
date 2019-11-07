a3e15ad5dacf4a1f583608e110979137f720cf6c
ADRIANO GONCALVES DE SOUZA <UZ96445@usiminas.com>
2019-11-07 10:00:36 -0300

    Refatoração

--- src/com/desafios/rename/RenameRepeatedStrings.java
+++ src/com/desafios/rename/RenameRepeatedStrings.java
@@ -6,30 +6,29 @@ import java.util.List;
 import java.util.Map;
 
 public class RenameRepeatedStrings {
-	
-	public static void main(String args[])	{
-		String[] users = {"Sara", "Adriano", "Mario", "Sara", "Gerald", "Sara", "Adriano"};
-		
-		//Usando HashMap conplexidade O(2n)
-		
+
+	public static void main(String args[]) {
+		String[] users = { "Sara", "Adriano", "Mario", "Sara", "Gerald", "Sara", "Adriano" };
+
+		// Usando HashMap conplexidade O(2n)
 		Map<String, Integer> usersMap = new HashMap<String, Integer>();
 		List<String> renamedUsers = new ArrayList<String>();
-		
-		for(String name : users) {
-			Integer countName = usersMap.get(name); //Retorna a quantidade de elementos na lista
-			if(countName == null) {
+
+		for (String name : users) {
+			Integer countName = usersMap.get(name); // Retorna a quantidade de elementos na lista
+			if (countName == null) {
 				usersMap.put(name, 1);
 				renamedUsers.add(name);
 			} else {
-				usersMap.put(name, ++countName); //Adiciona o elemento mais a quantidade de repetições
+				usersMap.put(name, ++countName); // Adiciona o elemento mais a quantidade de repetições
 				renamedUsers.add(name.concat(countName.toString()));
-			}		
-		}
-		
-		for(String name : renamedUsers ) {
-			System.out.println(name); //Imprime elementos renomeados
+			}
 		}
 
-	}	
+		for (String name : renamedUsers) {
+			System.out.println(name); // Imprime elementos renomeados
+		}
+
+	}
 
 }
